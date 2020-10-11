0c0b3d2e38d2e5ffa2bf36bdc6a4368913de24b4
Scott Kennedy <scottyzen@gmail.com>
2020-10-11 14:56:00 +0100

    Removed ids from froundend

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,158 @@
 <template>
-  <div class="container p-12 mx-auto">
-    <div>
-      <pre
-        v-if="data"
-        class="w-full text-sm bg-gray-800 text-white p-4 rounded"
-        >{{ data }}</pre
+  <div class="container">
+    <ul v-if="$store.state.todaysMatches" class=" mt-8">
+      <li
+        v-for="(match, i) in $store.state.todaysMatches.data.matches.slice(
+          0,
+          10
+        )"
+        :key="match.id"
+        class="flex justify-between flex-wrap mb-5"
       >
-    </div>
-    <ul class="mt-8">
-      <li v-for="user in data" :key="user.name">
-        {{ user.email }}
+        <span class="uppercase flex items-center text-gray-700 text-xs"
+          ><img
+            width="16"
+            height="16"
+            class="mr-1 object-contain mb-1"
+            :src="`/${match.homeTeam.id}.png`"
+            :alt="`${match.homeTeam.name} logo`"
+          />{{ match.homeTeam.name }} </span
+        ><span class="uppercase flex items-center text-gray-700 text-xs"
+          >{{ match.awayTeam.name }}
+          <img
+            width="16"
+            height="16"
+            class="ml-1 object-contain mb-1"
+            :src="`/${match.awayTeam.id}.png`"
+            :alt="`${match.awayTeam.name} logo`"
+        /></span>
+
+        <div class=" w-full bar shadow-inner h-3 rounded relative">
+          <span
+            :style="{
+              left: results[i] == null ? 50 + '%' : results[i] + '%',
+              animationDelay: `${Math.floor(Math.random() * 400) + 100}ms`,
+              animationDuration: `${Math.floor(Math.random() * 2500) + 1500}ms`
+            }"
+            :class="{ crunching: running }"
+            class=" pointer h-6 bg-gray-600 shadow w-1 block rounded -mt-2 absolute"
+          ></span>
+          <span style="left: 25%" class="line"></span>
+          <span style="left: 50%" class="line"></span>
+          <span style="left: 75%" class="line"></span>
+        </div>
       </li>
     </ul>
+
+    <button
+      class="rounded-md w-full mt-8 border-b border-gray-400 text-gray-700 bg-gray-300 p-2"
+      @click="run"
+    >
+      {{ running ? "Running" : "Predict all matches" }}
+    </button>
   </div>
 </template>
 
 <script>
+const brain = require("../node_modules/brain.js/src/index");
 export default {
   data() {
     return {
-      data: false
+      running: false,
+      results: []
     };
   },
-  mounted() {
-    var ref = this.$fireDb.ref("users");
-    ref.on("value", snapshot => {
-      const dbValue = snapshot.val();
-      this.data = dbValue;
-    });
+  methods: {
+    run() {
+      this.running = true;
+      const startTheTest = async () => {
+        let history = this.$store.state.matchData.data.matches;
+        let noDraws = history.filter(match => {
+          return match.score.winner != "DRAW";
+        });
 
-    // this.$fireDb.ref("users/" + 22).set({
-    //   username: "Ciara",
-    //   email: "kiearh@hotmail.com"
-    // });
+        let upcomming = this.$store.state.todaysMatches;
+        upcomming = upcomming.data.matches.slice(0, 10);
+
+        const trainingData = noDraws.map(match => {
+          return {
+            input: [match.homeTeam.id, match.awayTeam.id],
+            output: [this.convertResultToNumber(match.score.winner)]
+          };
+        });
+
+        const removeHomeAdvantage = noDraws.map(match => {
+          return {
+            input: [match.awayTeam.id, match.homeTeam.id],
+            output: [
+              this.convertResultToNumber(match.score.winner) == 1 ? 0 : 1
+            ]
+          };
+        });
+
+        const trainingDataWithNoHomeAdvantage = [
+          ...trainingData,
+          ...removeHomeAdvantage
+        ];
+
+        const net = await new brain.NeuralNetwork({
+          inputRange: 20,
+          hiddenLayers: [20, 20, 20],
+          learningRate: 0.1,
+          decayRate: 0.1
+        });
+        net.train(trainingDataWithNoHomeAdvantage, { iterations: 100 });
+
+        for (const match of upcomming) {
+          let result = await net.run([match.homeTeam.id, match.awayTeam.id]);
+          console.log(
+            `${match.homeTeam.name} vs. ${match.awayTeam.name} : ${result}`
+          );
+          this.results.push(result * 100);
+          this.running = false;
+        }
+      };
+      setTimeout(() => {
+        startTheTest();
+      }, 1);
+    },
+    convertResultToNumber(result) {
+      if (result == "DRAW") {
+        return 0.5;
+      }
+      return result == "AWAY_TEAM" ? 1 : 0;
+    }
   }
 };
 </script>
+
+<style>
+.spinner {
+  width: 20px;
+  height: 20px;
+  background: red;
+  display: inline-block;
+  animation: spin 600ms linear infinite forwards;
+}
+.bar {
+  background: linear-gradient(to right, #ff5122, #f0da56, #2fcd21);
+}
+.pointer {
+  transition: all 1000ms ease-in-out;
+  z-index: 9;
+}
+.line {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  display: inline-block;
+  width: 1px;
+  background: rgba(0, 0, 0, 0.2);
+}
+
+@keyframes spin {
+  100% {
+    transform: rotate(359deg);
+  }
+}
+</style>
